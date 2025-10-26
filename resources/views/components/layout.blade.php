50cbd260b619b6353aea6e23f72ccd5d4af7b6b0
rds-renan <renan.souza@datacube.com.br>
2025-10-26 14:59:57 -0300

    feat(ui): Change tags for new sistem
    Change the tags in layout.blade.php for blade components system.
    Change the name of the files in the resource from .html to .blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,13 +21,13 @@
         :class="{'dark bg-gray-900': darkMode === true}"
     >
         <!-- ===== Preloader Start ===== -->
-        <include src="./partials/preloader.html"></include>
+        <x-partials.preloader />
         <!-- ===== Preloader End ===== -->
 
         <!-- ===== Page Wrapper Start ===== -->
         <div class="flex h-screen overflow-hidden">
             <!-- ===== Sidebar Start ===== -->
-            <include src="./partials/sidebar.html"></include>
+            <x-partials.sidebar />
             <!-- ===== Sidebar End ===== -->
 
             <!-- ===== Content Area Start ===== -->
@@ -35,11 +35,11 @@
                 class="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto"
             >
                 <!-- Small Device Overlay Start -->
-                <include src="./partials/overlay.html" />
+                <x-partials.overlay />
                 <!-- Small Device Overlay End -->
 
                 <!-- ===== Header Start ===== -->
-                <include src="./partials/header.html" />
+                <x-partials.header />
                 <!-- ===== Header End ===== -->
 
                 <!-- ===== Main Content Start ===== -->
@@ -48,34 +48,34 @@
                         <div class="grid grid-cols-12 gap-4 md:gap-6">
                             <div class="col-span-12 space-y-6 xl:col-span-7">
                                 <!-- Metric Group One -->
-                                <include src="./partials/metric-group/metric-group-01.html" />
+                                <x-partials.metric-group.metric-group-01 />
                                 <!-- Metric Group One -->
 
                                 <!-- ====== Chart One Start -->
-                                <include src="./partials/chart/chart-01.html" />
+                                <x-partials.chart.chart-01 />
                                 <!-- ====== Chart One End -->
                             </div>
                             <div class="col-span-12 xl:col-span-5">
                                 <!-- ====== Chart Two Start -->
-                                <include src="./partials/chart/chart-02.html" />
+                                <x-partials.chart.chart-02 />
                                 <!-- ====== Chart Two End -->
                             </div>
 
                             <div class="col-span-12">
                                 <!-- ====== Chart Three Start -->
-                                <include src="./partials/chart/chart-03.html" />
+                                <x-partials.chart.chart-03 />
                                 <!-- ====== Chart Three End -->
                             </div>
 
                             <div class="col-span-12 xl:col-span-5">
                                 <!-- ====== Map One Start -->
-                                <include src="./partials/map-01.html" />
+                                <x-partials.map-01 />
                                 <!-- ====== Map One End -->
                             </div>
 
                             <div class="col-span-12 xl:col-span-7">
                                 <!-- ====== Table One Start -->
-                                <include src="./partials/table/table-01.html" />
+                                <x-partials.table.table-01 />
                                 <!-- ====== Table One End -->
                             </div>
                         </div>
