a9e31d0808b17872453234120664d34a6522b11e
rds-renan <renan.souza@datacube.com.br>
2025-11-16 14:20:08 -0300

    chore(users): Add roles users
    Perform the migration by adding the role column to the users table.
    Add seeder to create users with role

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin Hub',
+            'email' => 'admin@hubdocafe.rds.dev.br',
+            'password' => Hash::make('Admin#147'),
+            'role' => 'admin'
+        ]);
+
+        User::create([
+            'name' => 'Cliente Teste',
+            'email' => 'cliente@teste.com',
+            'password' => Hash::make('Client@258'),
+            'role' => 'client'
+        ]);
+
+        User::create([
+            'name' => 'Atendente Teste',
+            'email' => 'atendente@hubdocafe.rds.dev.br',
+            'password' => Hash::make('Atendente#369'),
+            'role' => 'employee'
+        ]);
+    }
+}
