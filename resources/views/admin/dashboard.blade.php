cf8e7c37896aa593855e5decc4c371acf21f6e63
rds-renan <renan.souza@datacube.com.br>
2025-11-16 21:25:07 -0300

    Fix(crm-ui): Fix home page CRM
    Fixing CRM HTML tags

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,13 +21,13 @@
         :class="{'dark bg-gray-900': darkMode === true}"
     >
         <!-- ===== Preloader Start ===== -->
-        <x-partials.preloader />
+        <x-admin.preloader />
         <!-- ===== Preloader End ===== -->
 
         <!-- ===== Page Wrapper Start ===== -->
         <div class="flex h-screen overflow-hidden">
             <!-- ===== Sidebar Start ===== -->
-            <x-partials.sidebar />
+            <x-admin.sidebar />
             <!-- ===== Sidebar End ===== -->
 
             <!-- ===== Content Area Start ===== -->
@@ -35,11 +35,11 @@
                 class="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto"
             >
                 <!-- Small Device Overlay Start -->
-                <x-partials.overlay />
+                <x-admin.overlay />
                 <!-- Small Device Overlay End -->
 
                 <!-- ===== Header Start ===== -->
-                <x-partials.header />
+                <x-admin.header />
                 <!-- ===== Header End ===== -->
 
                 <!-- ===== Main Content Start ===== -->
@@ -48,34 +48,34 @@
                         <div class="grid grid-cols-12 gap-4 md:gap-6">
                             <div class="col-span-12 space-y-6 xl:col-span-7">
                                 <!-- Metric Group One -->
-                                <x-partials.metric-group.metric-group-01 />
+                                <x-admin.metric-group.metric-group-01 />
                                 <!-- Metric Group One -->
 
                                 <!-- ====== Chart One Start -->
-                                <x-partials.chart.chart-01 />
+                                <x-admin.chart.chart-01 />
                                 <!-- ====== Chart One End -->
                             </div>
                             <div class="col-span-12 xl:col-span-5">
                                 <!-- ====== Chart Two Start -->
-                                <x-partials.chart.chart-02 />
+                                <x-admin.chart.chart-02 />
                                 <!-- ====== Chart Two End -->
                             </div>
 
                             <div class="col-span-12">
                                 <!-- ====== Chart Three Start -->
-                                <x-partials.chart.chart-03 />
+                                <x-admin.chart.chart-03 />
                                 <!-- ====== Chart Three End -->
                             </div>
 
                             <div class="col-span-12 xl:col-span-5">
                                 <!-- ====== Map One Start -->
-                                <x-partials.map-01 />
+                                <x-admin.map-01 />
                                 <!-- ====== Map One End -->
                             </div>
 
                             <div class="col-span-12 xl:col-span-7">
                                 <!-- ====== Table One Start -->
-                                <x-partials.table.table-01 />
+                                <x-admin.table.table-01 />
                                 <!-- ====== Table One End -->
                             </div>
                         </div>
