de6b438fbf87df89da9ff10e541202bf8946d2c2
rds-renan <renan.souza@datacube.com.br>
2025-11-16 21:24:08 -0300

    Fix(products): Fix description
    Fix the product description that wasn't being sent to the home page.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function __invoke()
     {
-        $products = ProductModel::select('id','name','price','image','categoria','badge_tag')->get();
+        $products = ProductModel::select('id','name','description','price','image','categoria','badge_tag')->get();
         return view('frontend.home', compact('products'));
     }
 }
